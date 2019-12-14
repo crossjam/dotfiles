2ffe31d3c1e24d749a5531c070d3f506de1972ec
Brian Dennis <briandennis@datamachines.io>
2019-12-13 20:49:33 -0500

    Fix up .emacs.d directory creation

--- HOMELY.py
+++ HOMELY.py
@@ -11,7 +11,7 @@ home_dir = Path.home()
 emacs_init_dir = home_dir / ".emacs.d"
 
 if not emacs_init_dir.exists():
-    mkdir(emacs_init_dir)
+    mkdir(str(emacs_init_dir))
 
 symlink("emacs_init.el", str(emacs_init_dir / "init.el"))
 
