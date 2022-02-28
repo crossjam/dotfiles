9a049e90ecc9d5da9d54a4c5d0d70130aec02bfa
Brian Dennis <bmd+github@crossjam.net>
2022-02-28 12:05:39 -0500

    Specify the right source for the ssh key bind mount

--- test/install_test.sh
+++ test/install_test.sh
@@ -5,5 +5,5 @@ docker build -f Dockerfile . -t dotfiles-test
 docker run --rm -it \
        --env DOTFILES_BRANCH=${DOTFILES_BRANCH:-master} \
        --mount "type=bind,source=$PWD/install_dotfiles.sh,target=/home/crossjam/install_dotfiles.sh" \
-       --mount "type=bind,source=$HOME/.ssh/crossjam.ecdsa,target=${SSH_KEY_PATH:-/home/crossjam/.ssh/id_ecdsa}" \
+       --mount "type=bind,source=${SSH_KEY_PATH:-/home/crossjam/.ssh/id_ecdsa},target=/home/crossjam/.ssh/id_ecdsa" \
        dotfiles-test
