c750ebf794b31a0e935650162ccda4e251b92a35
Brian Dennis <bmd+github@crossjam.net>
2022-02-27 20:17:19 -0500

    Initial attempt to incorporate homebrew then pyenv install

--- HOMELY.py
+++ HOMELY.py
@@ -1,9 +1,13 @@
+import platform
 import shutil
+import tempfile
 import time
 
 from pathlib import Path
 
-from homely.files import symlink, mkdir
+from homely.files import symlink, mkdir, download
+from homely.install import installpkg
+from homely.system import execute, haveexecutable
 from homely.ui import head, note
 
 home_dir = Path.home()
@@ -14,6 +18,41 @@ mkdir("~/.emacs.d")
 
 if not dotfiles_old_dir.exists():
     dotfiles_old_dir.mkdir()
+
+installpkg("curl")
+
+HOMEBREW_INSTALL_SCRIPT = (
+    "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+)
+
+with head("homebrew"):
+    if not haveexecutable("brew"):
+        install_system = platform.system()
+        if install_system == "Linux":
+            note("need to install Linux homebrew")
+            with tempfile.TemporaryFile() as install_sh_tmp:
+                note(f"Downloading brew install script to: {install_sh_tmp}")
+                download(HOMEBREW_INSTALL_SCRIPT, install_sh_tmp)
+                note("Executing brew install script")
+                execute(["/bin/bash", "-c", install_sh_tmp])
+                # execute(["curl", "-fsSL", "-o", install_sh_tmp, HOMEBREW_INSTALL_SCRIPT])
+        elif install_sytem == "Darwin":
+            note("need to install Mac homebrew")
+            with tempfile.NamedTemporaryFile() as install_sh_tmp:
+                note(f"Downloading brew install script to: {install_sh_tmp}")
+                download(HOMEBREW_INSTALL_SCRIPT, install_sh_tmp)
+                note("Executing brew install script")
+                execute(["/bin/bash", "-c", install_sh_tmp])
+        else:
+            note("Unknown brew platform")
+    else:
+        note("homebrew already installed")
+
+with head("pyenv"):
+    if haveexecutable("brew"):
+        execute(["brew", "install", "pyenv"])
+        execute(["brew", "install", "pyenv-virtualenv"])
+
 
 INSTALL_DOTFILES = [
     ("screenrc", ".screenrc"),
@@ -30,11 +69,12 @@ with head("Processing potentially preexisting targets."):
         dot_file_path = Path("~/dotfiles").expanduser() / dot_file
         note(f"Checking {orig_file_path}")
         if orig_file_path.exists() and not (orig_file_path.samefile(dot_file_path)):
-            dst_file_path = (dotfiles_old_dir / f"{orig_file_path.name}_{int(time.time())}")            
+            dst_file_path = (
+                dotfiles_old_dir / f"{orig_file_path.name}_{int(time.time())}"
+            )
             note(f"Original file: {orig_file_path} -> {dst_file_path}")
             shutil.move(orig_file_path, dst_file_path)
 
 with head("Installing dotfiles."):
     for dot_file, orig_file in INSTALL_DOTFILES:
         symlink(dot_file, orig_file)
-
