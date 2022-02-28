571b304be578e9897b90b308f58364b55da0fa81
Brian Dennis <bmd+github@crossjam.net>
2022-02-28 11:51:15 -0500

    Allow specification of dotfiles branch by envvar, possible fixups for linuxbrew git pull failures

--- test/install_dotfiles.sh
+++ test/install_dotfiles.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+export NONINTERACTIVE=t
+
+git config --global core.compression 0
+git config --global http.postBuffer 1048576000
 
 pip install --user homely
 PATH=$PATH:$HOME/.local/bin
 git clone git@github.com:crossjam/dotfiles.git
+cd dotfiles
+git checkout ${DOTFILES_BRANCH:-master}
+cd ..
 homely add dotfiles
 homely update
--- test/install_test.sh
+++ test/install_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+docker build -f Dockerfile . -t dotfiles-test
+
 docker run --rm -it \
+       --env DOTFILES_BRANCH=${DOTFILES_BRANCH:-master} \
        --mount "type=bind,source=$PWD/install_dotfiles.sh,target=/home/crossjam/install_dotfiles.sh" \
        --mount "type=bind,source=$HOME/.ssh/crossjam.ecdsa,target=/home/crossjam/.ssh/id_ecdsa" \
        dotfiles-test
