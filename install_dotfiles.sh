3753c68e3aa9cfdaff4c195e7ceadb70aab82699
Brian Dennis <bmd+github@crossjam.net>
2022-03-02 10:29:16 -0500

    Update name of default branch to checkout

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -6,8 +6,7 @@ git config --global http.postBuffer 1048576000
 
 pip install --user homely
 PATH=$PATH:$HOME/.local/bin
-git clone git@github.com:crossjam/dotfiles.git
 cd dotfiles
-git checkout ${DOTFILES_BRANCH:-master}
+git checkout ${DOTFILES_BRANCH:-main}
 cd ..
 homely add dotfiles
--- test/install_dotfiles.sh
+++ test/install_dotfiles.sh
@@ -8,6 +8,6 @@ pip install --user homely
 PATH=$PATH:$HOME/.local/bin
 git clone git@github.com:crossjam/dotfiles.git
 cd dotfiles
-git checkout ${DOTFILES_BRANCH:-master}
+git checkout ${DOTFILES_BRANCH:-main}
 cd ..
 homely add dotfiles
