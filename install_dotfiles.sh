daa3e6133720e9bb7d18856b962b87dd2147869e
Brian Dennis <bmd+github@crossjam.net>
2022-03-02 10:07:28 -0500

    Initial commit of install script

---
+++ install_dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export NONINTERACTIVE=t
+
+git config --global core.compression 0
+git config --global http.postBuffer 1048576000
+
+pip install --user homely
+PATH=$PATH:$HOME/.local/bin
+git clone git@github.com:crossjam/dotfiles.git
+cd dotfiles
+git checkout ${DOTFILES_BRANCH:-master}
+cd ..
+homely add dotfiles
