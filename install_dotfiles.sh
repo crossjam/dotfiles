79e4564d7faf46fc7fb8104acbcf65482eeaf271
Brian Dennis <bmd+github@crossjam.net>
2022-06-12 23:17:37 -0400

    Ensure PYTHONUSERBASE envvar is exported correctly

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,7 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 # export GIT_HTTP_MAX_REQUEST_BUFFER=100M
 # export GIT_CORE_COMPRESSION=0
 
-PYTHONUSERBASE=$HOME/.local /usr/bin/python3 -m pip install pipx
+export PYTHONUSERBASE=$HOME/.local
+/usr/bin/python3 -m pip install pipx
 PATH=$PATH:$HOME/.local/bin
 pipx install homely
 homely add dotfiles
