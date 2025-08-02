3620ea8076afef7200b739a576d559301818f770
Brian Dennis <bmd+github@crossjam.net>
2025-08-02 14:59:13 -0400

    Add some commentary

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Presumably if you just do a
+# git clone https://github.com/crossjam/dotfiles
+# cd dotfiles
+# and run this script things should install
+
 export NONINTERACTIVE=t
 export DEBIAN_FRONTEND=noninteractive
 # export GIT_HTTP_MAX_REQUEST_BUFFER=100M
@@ -6,6 +11,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 export PYTHONUSERBASE=$HOME/.local
 
+# Bootstrap pipx into the account
 # On macOS, it's best to install the python.org version of python
 if [[ $OSTYPE == "darwin"* ]]; then
     PIP_REQUIRE_VIRTUALENV=false /Library/Frameworks/Python.framework/Versions/Current/bin/python3 -m pip install --user pipx
@@ -17,10 +23,12 @@ PATH=$PATH:$HOME/.local/bin
 # pipx install homely
 # pipx install git+ssh://git@github.com/crossjam/homely.git
 
+# Bootstrap uv into the account with pipx
 if ! pipx list | grep -q "package uv"; then
     pipx install uv
 fi
 
+# Bootstrap homely into the account with uv
 # Personal fork with fixes for asyncio deprecations
 if ! uv tool list | grep -q homely; then
     uv tool install --with requests --managed-python "homely @ git+https://github.com/crossjam/homely"
